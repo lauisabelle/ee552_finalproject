d78a9769123481d5b5db4538ef96e7c13bbe422d
Izzy <macbook@is-macbook-pro.lan>
2023-03-26 23:45:10 -0700

    finished edits but haven't tested yet

--- processing_elements.sv
+++ processing_elements.sv
@@ -9,11 +9,11 @@
 `define SUM_WIDTH 13
 `define MAX_NUM_WEIGHTS 32 // local storage upper bound
 `define MAX_NUM_INPUTS 32
+`define IFMAP_MEM_ID 10
 
 import SystemVerilogCSP::*;
 
-
-module pe_partial (interface in, interface out1, interface out2);
+module pe_partial (interface in, interface out);
   // Packet Format
   // |   29 - 26    |    25   |   24 - 0 |
   // | dest address |  opcode |   data   |
@@ -33,22 +33,16 @@ module pe_partial (interface in, interface out1, interface out2);
 
   // Packet storage
   logic [ADDR_START:0] packet;
-  logic [ADDR_START - ADDR_END + 1:0] dest_address;
+  logic [ADDR_START - ADDR_END + 1:0] dest_address = 0;
   logic opcode;
   logic signed [DATA_START - DATA_END + 1:0] data;
-
-
-  logic OUTPUT_DIM = IFMAP_SIZE - FILTER_SIZE + 1
-  logic NUM_INPUTS = OUTPUT_DIM * OUTPUT_DIM; // max num inputs PE will receive
+  logic [4:0] OUTPUT_DIM = IFMAP_SIZE - FILTER_SIZE + 1
   logic signed [WEIGHT_WIDTH-1:0] weights_mem [MAX_NUM_WEIGHTS:0]; // array of MAX_NUM_WEIGHTS 8-bit elements
   logic signed [MAX_NUM_INPUTS:0] inputs_mem; // array of MAX_NUM_INPUTS 1-bit elements
-
-
   logic signed [SUM_WIDTH:0] partial_sum;
 
   // Pointers
   logic [$clog2(MAX_NUM_WEIGHTS) - 1:0] wstore_ptr = 0;
-  logic [$clog2(MAX_NUM_INPUTS) - 1:0] istore_ptr = 0;
   logic [$clog2(MAX_NUM_INPUTS) - 1:0] isum_ptr = 0
 
   always begin
@@ -72,50 +66,50 @@ module pe_partial (interface in, interface out1, interface out2);
       weights_mem[wstore_ptr+1] = data[15:8];
       wstore_ptr += 2;
     end
+    // Once inputs are recieve, start the partial sum
     else if(opcode == INPUT) begin
+
+      // Prepare to process the next set of inputs
+      iptr = 0;
+      isum_ptr = 0;
 
       // Store received 1-bit inputs in the memory
       inputs_mem[iptr] = data;
-      istore_ptr += (DATA_END - DATA_START + 1) 
 
-      // ??? how to time this --> Compute partial sum after weights have been received
-      if(wptr == (FILTER_SIZE * WEIGHT_WIDTH)) begin
-          for(int wptr = 0, iptr = isum_ptr; iptr < KERNEL_SIZE; w++, i++) begin
-            partial_sum += (inputs_mem[iptr] * weights_mem[wptr]); // can sub i, j for wptr iptr? Should I update them?
-          end
-
+      // Begin partial sum calculations
+      // Calculate in increments of FILTER_SIZE
+      for(int w = 0, i = isum_ptr; i < FILTER_SIZE; w++, i++) begin
+        partial_sum += (inputs_mem[i] * weights_mem[w]);
       end
-      
-      if(ctr+1 % OUTPUT_DIM == 0) begin
-        ctr = 0; // move to the next "row" of inputs
-        iptr = (iptr + FILTER_SIZE) % 32; // move to the next "row" of inputs
-      end
-      else begin
-        iptr = (iptr + 1) % 32; // slide the window of inputs by 1
-      end
-    
-    
 
-
-
-
-
-
-
-
-
-      // Repacketize data
-      packet[ADDR_END:ADDR_START] = DEST_PE_ID;
-      // output_idx = packet[IDX_END:IDX_START]; STAYS THE SAME
+      // Send data to SPE
+      packet[ADDR_END:ADDR_START] = dest_address;
       packet[OPCODE] = 0; // irrelevant for the combining PE, use 0 as dummy
       packet[DATA_END:DATA_START] = partial_sum;
-
-        //Communication action Send is about to start
+      dest_address = (dest_address + 1) % FILTER_SIZE; // cycle through all of the SPE's 0 - 4
       $display("Start sending in module %m. Simulation time = %t", $time);
       $display("Sending data = %d", data);
-      fork
-        out.send(packet);
-      join
+      out.send(packet);
+      $display("Finished sending in module %m. Simulation time = %t", $time);
+      #BL;
+
+      // Prepare to read the next set of data
+      if(isum_ptr + 1 % OUTPUT_DIM == 0) begin
+        isum_ptr = iptr + FILTER_SIZE; // move to the next "row" of inputs
+      end
+      else begin
+        isum_ptr = isum_ptr + 1; // slide the window of inputs by 1
+      end
+
+      // Request more inputs from I_MEM
+      packet[ADDR_END:ADDR_START] = IFMAP_MEM_ID;
+      packet[OPCODE] = 0; // opcode
+      packet[DATA_END:DATA_START] = 0; // irrelevant
+
+      //Communication action Send is about to start
+      $display("Start sending in module %m. Simulation time = %t", $time);
+      $display("Sending data = %d", data);
+      out.send(packet);
       //Communication action Send is finished
       $display("Finished sending in module %m. Simulation time = %t", $time);
       #BL;//Backward Latency: Delay from the time data is delivered to the time next input can be accepted
