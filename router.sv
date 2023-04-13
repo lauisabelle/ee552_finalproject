e019b581fe15029b103b984c1b677fe7cee7afd7
MengyuSun <mengyusu@usc.edu>
2023-04-13 12:08:22 -0700

    updated router.sv and a new noc.sv file

--- router.sv
+++ router.sv
@@ -277,6 +277,47 @@ module decision_making (interface in, interface up, interface down, interface le
 endmodule
 
 
+//Sample data_bucket module
+module data_bucket (interface r);
+  parameter WIDTH = 8;
+  parameter BL = 0; //ideal environment    backward delay
+  logic [WIDTH-1:0] ReceiveValue = 0;
+  
+//   //Variables added for performance measurements
+//   real cycleCounter=0, //# of cycles = Total number of times a value is received
+//        timeOfReceive=0, //Simulation time of the latest Receive 
+//        cycleTime=0; // time difference between the last two receives
+//   real averageThroughput=0, averageCycleTime=0, sumOfCycleTimes=0;
+  always
+  begin
+	
+	//add a display here to see when this module starts its main loop
+    // $display("*** %m started @ time = %0d",$time);
+
+    // timeOfReceive = $time;
+	
+	//Communication action Receive is about to start
+    // $display("%m started receiving @ time = %0d", $time);
+    r.Receive(ReceiveValue);
+    // $display("%m finished receiving @ time = %0d, data = %b", $time, ReceiveValue);
+	//Communication action Receive is finished
+    
+	#BL;
+    // cycleCounter += 1;		
+    // //Measuring throughput: calculate the number of Receives per unit of time  
+    // //CycleTime stores the time it takes from the begining to the end of the always block
+    // cycleTime = $time - timeOfReceive; // the difference of time between now and the last receive
+    // averageThroughput = cycleCounter/$time; 
+    // sumOfCycleTimes += cycleTime;
+    // averageCycleTime = sumOfCycleTimes / cycleCounter;
+    // // $display("Execution cycle= %d, Cycle Time= %d, Average CycleTime=%f, Average Throughput=%f", cycleCounter, cycleTime, averageCycleTime, averageThroughput);
+	
+	
+  end
+
+endmodule
+
+
 
 
 
